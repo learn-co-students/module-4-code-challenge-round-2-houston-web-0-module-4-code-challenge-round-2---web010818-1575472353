1cfdb62f86ec662d597243782eb61b74e496b8ee
Patrick Alexander <patrick_alexander@mail.com>
2019-12-04 10:25:45 -0600

    Take Two

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Search = () => {
+const Search = (props) => {
   return (
     <div className="ui huge fluid icon input">
-      <input
+      <input onChange={(e) => props.handleChange(e)}
         type="text"
         placeholder={"Search your Recent Transactions"}
       />
